b216fb6063fc20faf08cd14e43bbb0c205649a1d
StevenGX12 <guanxiao2000@gmail.com>
2023-06-16 04:20:27 +0800

    updated stats API

--- handlers/statistics.js
+++ handlers/statistics.js
@@ -9,23 +9,16 @@ const searchSpecies = async (request, response) => {
       const { species, startTime, endTime, sightingLocation } = request.query;
   
       let query = `
-        WITH latest_sightings AS (
-          SELECT postid, MAX(sightinglocation) AS sightinglocation
-          FROM posts
-          WHERE title = $1 AND verified = true
-            AND sightingtime >= $2 AND sightingtime <= $3
-            ${sightingLocation !== '' ? 'AND sightinglocation = $4' : ''}
-          GROUP BY postid
-          ORDER BY MAX(sightingtime) DESC
-          LIMIT 1
-        )
         SELECT COUNT(*) AS count, MAX(posts.sightingtime) AS latest_sightingtime,
-          latest_sightings.sightinglocation AS latest_sightinglocation
+          (SELECT sightinglocation FROM posts
+           WHERE title = $1 AND verified = true
+             AND sightingtime >= $2 AND sightingtime <= $3
+             ${sightingLocation !== '' ? 'AND sightinglocation = $4' : ''}
+           ORDER BY sightingtime DESC
+           LIMIT 1) AS latest_sightinglocation
         FROM posts
-        JOIN latest_sightings ON posts.postid = latest_sightings.postid
         WHERE title = $1 AND verified = true
           AND sightingtime >= $2 AND sightingtime <= $3
-        GROUP BY latest_sightings.sightinglocation
       `;
   
       const values = [species, startTime, endTime];
@@ -50,6 +43,7 @@ const searchSpecies = async (request, response) => {
       response.status(500).json({ error: 'Internal server error' });
     }
   };
+  
   
 
 const searchClass = async (request, response) => {
