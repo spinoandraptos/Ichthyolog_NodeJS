b0e15cc75ef14e830ce4d91bc2ae196e285cfbc0
spinoandraptos <spinoandraptos@gmail.com>
2023-07-22 16:38:12 +0800

    Updated upload post API

--- handlers/post.js
+++ handlers/post.js
@@ -130,11 +130,7 @@ const viewUserPosts = async (request, response) => {
           else if(result.rowCount == 1){
             var blank = ''
             const picture = result.rows[0].profilepic
-            db.dbConnect().query('UPDATE users SET "totalposts" = "totalposts" + 1, "level" = "level" + 1 WHERE userid = $1'), [userid], (error, result) => {
-              if (error) {
-                response.send(error.message)
-              }
-              else if(result.rowCount == 1){
+
                 db.dbConnect().query('INSERT INTO posts (userid, authorname, title, description, time, sightinglocation, sightingtime, imageurl, authorpicurl, class, _order, family, genus, species) VALUES ($1, $2, $3, NULLIF($4, $14) now(), NULLIF($5, $14), $6, $7, $8, NULLIF($9,$14), NULLIF($10,$14), NULLIF($11,$14), NULLIF($12,$14), NULLIF($13,$14))', 
                   [userid, authorname, title, description, sightingLocation, sightingTime, imageURL, picture, _class, order, family, genus, species, blank], 
                   (error, result) => {
@@ -143,11 +139,8 @@ const viewUserPosts = async (request, response) => {
                   }
                   else {response.status(201).send(`Post with title: ${title} added`)}
                 })
+   
               }
-              else {
-                response.status(404).send('User not found')
-              }  
-          }}
           else {
             response.status(404).send('User not found')
           }
